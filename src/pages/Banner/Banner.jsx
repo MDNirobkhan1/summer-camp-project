b1f7021fe612a4b8e64bcda381f4927412242b01
MDNirobkhan1 <nirob937612@gmail.com>
2023-06-07 06:29:05 +0600

    the banner design added

---
+++ src/pages/Banner/Banner.jsx
@@ -0,0 +1,76 @@
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
+import logo1 from '../../assets/Neg 2.jpg'
+import logo2 from '../../assets/slid.jpg'
+import logo3 from '../../assets/55.jpg'
+
+
+
+const Banner = () => {
+    return (
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+            <SwiperSlide className="relative ">
+
+                <div>
+                    <div className=' absolute  h-full flex gap-40  items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] left-0 bottom-0 p-8'>
+                        <div className='text-white ml-20'>
+                            <h1 className='text-4xl font-bold'> Photography School...</h1>
+                            <p>25 BEST PHOTOGRAPHY SCHOOLS IN THE WORLD IN 2023 BY COUNTRY.</p>
+                            <div className='mt-8'>
+                                <button className="btn btn-primary">Red More</button>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div>
+                        <img className="h-[600px] w-full" src={logo1} alt="" />
+                    </div>
+
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className="relative ">
+
+                <div>
+                    <div className=' absolute  h-full flex gap-40  items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] left-0 bottom-0 p-8'>
+                        <div className='text-white ml-20'>
+                            <h1 className='text-4xl font-bold'> Photography School...</h1>
+                            <p>25 BEST PHOTOGRAPHY SCHOOLS IN THE WORLD IN 2023 BY COUNTRY.</p>
+                            <div className='mt-8'>
+                                <button className="btn btn-primary">Red More</button>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div>
+                        <img className="h-[600px] w-full" src={logo2} alt="" />
+                    </div>
+
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className="relative ">
+
+                <div>
+                    <div className=' absolute  h-full flex gap-40  items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] left-0 bottom-0 p-8'>
+                        <div className='text-white ml-20'>
+                            <h1 className='text-4xl font-bold'> Photography School...</h1>
+                            <p>25 BEST PHOTOGRAPHY SCHOOLS IN THE WORLD IN 2023 BY COUNTRY.</p>
+                            <div className='mt-8'>
+                                <button className="btn btn-primary">Red More</button>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div>
+                        <img className="h-[600px] w-full" src={logo3} alt="" />
+                    </div>
+
+                </div>
+            </SwiperSlide>
+
+        </Swiper>
+    );
+};
+
+export default Banner;
--- src/pages/Home/Shared/Footer/Footer.jsx
+++ src/pages/Home/Shared/Footer/Footer.jsx
@@ -4,33 +4,39 @@ import logo from '../../../../assets/55102_1477681363.jpg'
 
 const Footer = () => {
     return (
-        <div className="mt-10">
+        <div className="mt-2">
             <footer className="footer p-10 20 bg-slate-900 text-white ">
-            <div>
-               <img src={logo} alt="" />
-                <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
-            </div>
-            <div>
-                <span className="footer-title">Services</span>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </div>
-            <div>
-                <span className="footer-title">Company</span>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </div>
-            <div>
-                <span className="footer-title">Legal</span>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
-            </div>
-        </footer>
+                <div>
+                    <img src={logo} alt="" />
+                    <p>Photography School<br />Providing reliable tech since 1992</p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+
+            </footer>
+            <footer className="footer footer-center p-4 bg-slate-900 text-white ">
+                <div>
+                    <p>Copyright © 2023 - All right reserved by Photography School</p>
+                </div>
+            </footer>
         </div>
     );
 };
