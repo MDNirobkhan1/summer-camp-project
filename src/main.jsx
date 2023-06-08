61b09dd2a08cdc6366857aef2f78d2cdf7c02a4d
MDNirobkhan1 <nirob937612@gmail.com>
2023-06-08 11:10:53 +0600

    auth providr file added

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Routes';
+import AuthProvider from './providers/AuthProvider';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='max-w-screen-xl bg-violet-200 mx-auto'>
-      <RouterProvider router={router} />
-    </div>
+    <AuthProvider>
+      <div className='max-w-screen-xl bg-violet-200 mx-auto'>
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
   </React.StrictMode>,
 )
